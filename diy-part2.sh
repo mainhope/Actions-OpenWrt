93b493738671dfcf9b2639dc8fde539ac840708c
mainhope <44607052+mainhope@users.noreply.github.com>
2021-06-27 16:22:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,8 +13,5 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# 添加luci-app-packages and small
-cd package/lean
-git clone https://github.com/mainhope/openwrt-luci-packages.git
-git clone https://github.com/mainhope/openwrt-luci-packages-small.git
+
 
